30247334b5eaac82a4a71871c2540a0754b9564a
MohammedAmaanQuraishi <mohammed.amaan.quraishi.12@gmail.com>
2026-02-05 15:03:09 +0530

    message

--- day 4  DS_AI_internship/Task-3.py
+++ day 4  DS_AI_internship/Task-3.py
@@ -8,8 +8,8 @@ friend_a= {"Python", "Cooking", "Hiking", "Movies"}
 friend_b= {"Hiking", "Gaming", "Photography", "Python"}
 
 shared_interests =friend_a & friend_b #intersection
-all_interests =friend_a  | friend_b    #union
-unique_to_a =friend_a    - friend_b      #Difference
+all_interests =friend_a| friend_b    #union
+unique_to_a =friend_a - friend_b      #Difference
 
 # Displaying Intersection
 print("Intersection of friend a and friend b are ",shared_interests)
