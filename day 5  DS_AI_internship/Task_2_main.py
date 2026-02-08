5dc9bea9373db4e7646dd8740a8e58513de1e450
MohammedAmaanQuraishi <mohammed.amaan.quraishi.12@gmail.com>
2026-02-08 07:02:01 +0530

    day6 task

--- day 5  DS_AI_internship/Task_2_main.py
+++
@@ -1,9 +0,0 @@
-import Task_2_math_operations as mo #importing the module
-
-print(mo.power(2, 10)) #Calling the power functions
-
-numbers = [10, 20, 30, 40]
-print(mo.average(numbers)) #importing the module#calling average function
-
-
-
